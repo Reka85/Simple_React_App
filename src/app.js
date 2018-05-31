5a14949a6b84e8535e12f2187bc6553a2ecf76da
Reka Kulinyi <reekaa85@gmail.com>
2018-05-31 16:55:09 +0200

    initial setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "normalize.css/normalize.css";
+import './styles/styles.scss';
+
+//run yarn install to install dependencies
+
+
+
+ReactDOM.render(<p>Simple React App</p>, document.getElementById('app'));
